d59a9724dc013197bf082410af57c0211a657656
muzammil-85 <muzukondeth85@gmail.com>
2021-10-26 07:22:47 +0530

    stable version

--- Takeshot/base/models.py
+++ Takeshot/base/models.py
@@ -1,7 +1,17 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(unique=True, null=True)
+    bio = models.TextField(null=True)
+
+    avatar = models.ImageField(null=True, default="avatar.svg")
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
 
 class Topic(models.Model):
     name = models.CharField(max_length=200)
@@ -10,30 +20,32 @@ class Topic(models.Model):
         return self.name
 
 
-# THE CLASS ROOM BECAME A TABLE AND THE VARIABLE BECOME A COLUMN NAME
 class Room(models.Model):
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
-    description = models.TextField(null=True,blank=True)
-    participants = models.ManyToManyField(User,related_name='participants',blank=True)
+    description = models.TextField(null=True, blank=True)
+    participants = models.ManyToManyField(
+        User, related_name='participants', blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ['-updated', '-created'] # order of the posted field
+        ordering = ['-updated', '-created']
 
     def __str__(self):
         return self.name
 
-class Message(models.Model): # this class is store message of users
+
+class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    room = models.ForeignKey(Room, on_delete=models.CASCADE) # it can be set SET_NULL (it will set null and data remain in server) value instead of CASCADE (delete everything from the server)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
     body = models.TextField()
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ['-updated', '-created'] # order of the posted field
+        ordering = ['-updated', '-created']
+
     def __str__(self):
         return self.body[0:50]
