7c8d094cb60a5aa7c1b5f29f389ca8dfda778e11
muzammil-85 <muzukondeth85@gamil.com>
2022-01-27 21:49:31 +0530

    ready to deploy

--- Takeshot/settings.py
+++ Takeshot/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3#av2c6nptlbbb6^muqkchu&fe3wv&n$t2+g$v!ir-f5%doocb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://talkshot.herokuapp.com/','localhost']
 
 
 # Application definition
