41900e8e75ee967031ec4a152879f8af66bd15b5
muzammil-85 <muzukondeth85@gamil.com>
2022-01-27 18:03:41 +0530

    everything setted

--- Takeshot/settings.py
+++ Takeshot/settings.py
@@ -16,6 +16,9 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
