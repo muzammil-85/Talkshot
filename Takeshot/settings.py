1605056ec0a866b60473265bf64d06ff7c9de5a6
muzu-85 <54140255+muzammil-85@users.noreply.github.com>
2022-02-11 15:37:15 +0530

    Update settings.py

--- Takeshot/settings.py
+++ Takeshot/settings.py
@@ -19,12 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3#av2c6nptlbbb6^muqkchu&fe3wv&n$t2+g$v!ir-f5%doocb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
